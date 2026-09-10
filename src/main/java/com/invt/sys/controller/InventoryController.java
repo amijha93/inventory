9afdab602296e998eb3a7087e67b5da3de562c71
amitjha93 <amit93.dbg@gmail.com>
2026-09-10 15:19:32 +0530

    Inventory controller added and implemented product findAll and findById method
    Implement JPARepo and connected Inventory database

--- src/main/java/com/invt/sys/controller/InventoryController.java
+++ src/main/java/com/invt/sys/controller/InventoryController.java
@@ -1,4 +1,30 @@
 package com.invt.sys.controller;
-@Deprecated
+
+import com.invt.sys.model.Product;
+import com.invt.sys.repo.InventoryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class InventoryController {
+    @Autowired
+    private InventoryRepo repo;
+    @GetMapping("/inventories")
+    public ResponseEntity<List<Product>> getProductList() {
+
+        return ResponseEntity.ok(repo.findAll());
+    }
+    @GetMapping("/inventory")
+    public ResponseEntity<Product> getProductById(@RequestParam int id) {
+
+        return ResponseEntity.ok(repo.findById(id).get());
+    }
 }
